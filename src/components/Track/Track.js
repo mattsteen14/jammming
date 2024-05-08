d11b2b575d3ae772bf8f60704bc4f0cc7ddbcda2
mattsteen14 <mattsteen14@me.com>
2024-05-08 14:34:05 +0100

    Track / TrackList.

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,6 +3,16 @@ import "./Track.css";
 
 const Track = (props) => {
 
+    return (
+        <div className="Track">
+            <div className="Track-information">
+                <h3>{props.track.name}</h3>
+                <p>
+                    {props.track.artist} | {props.track.album}
+                </p>
+            </div>
+        </div>
+    )
 }
 
 export default Track;
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -3,7 +3,21 @@ import "./TrackList.css";
 import Track from "../Track/Track";
 
 const TrackList = (props) => {
-
-}
+    return (
+        <div className="TrackList">
+            {props.tracks.map((track) => {
+                return (
+                    <Track 
+                    track={track}
+                    key={track.id}
+                    onAdd={props.onAdd}
+                    isRemoval={props.isRemoval}
+                    onRemove={props.onRemove}
+                    />
+                );
+            })}
+        </div>
+    );
+};
 
 export default TrackList;
